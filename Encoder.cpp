2690c232fe1546c2aee546a9bb91ddac52a161a3
Réthalmi Dávid <david.rethalmi@gmail.com>
2018-06-23 13:59:09 +0200

    Encoding is working now and Encoding and Decoding is separate.

--- Encoder.cpp
+++ Encoder.cpp
@@ -13,13 +13,36 @@ Encoder::~Encoder()
 
 void Encoder::EncodeIt(const string& input, json dictionary)
 {
+    string encodedString = "";
+    int badCharacters = 0;
 	for (unsigned int i = 0; i < input.length(); i++)
 	{
-		string actual(1, input[i]);
-        if(dictionary[actual.c_str()].is_null() != true)
-        cout << dictionary[actual.c_str()];
+        string actual(1, input[i]);
+        if(dictionary.find(actual) == dictionary.end())
+        {
+            badCharacters++;
+        }
+
 	}
-	cout << endl;
+    if(badCharacters == 0)
+    {
+        for (unsigned int i = 0; i < input.length(); i++)
+        {
+            string actual(1, input[i]);
+            if(dictionary.find(actual) != dictionary.end())
+            {
+                string encodedChar = dictionary[actual];
+                encodedString += encodedChar;
+                //cout << dictionary[actual.c_str()];
+            }
+        }
+        //cout << endl;
+        cout << "Result: " << encodedString << endl;
+    }
+    else
+    {
+        cout << "Wrong character in the input, unavailable to encode it!" << endl;
+    }
 }
 
 
