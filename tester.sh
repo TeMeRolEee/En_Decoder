fcf46208557de1c0a5b116c51d46e623ef48510f
Réthalmi Dávid <david.rethalmi@gmail.com>
2018-09-02 21:50:31 +0200

    Tester added, it tests both filemode and console mode

---
+++ tester.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd cmake-build-debug/
+
+echo "Console outputs are stored here:
+ ~/Academy/En_Decoder/cmake-build-debug/testerEncodeResult.txt
+ ~/Academy/En_Decoder/cmake-build-debug/testerDecodeResult.txt"
+
+(./En_Decoder dict.json -e tesztszovegamitrohadtullekenekodolniaaprogramnakdenagyonamhehehuhaezhosszuleszxd) >> ~/Academy/En_Decoder/cmake-build-debug/testerEncodeResult.txt
+
+(./En_Decoder dict.json -d n0m0re#asdops#asdw3a4trulz#asdw3a4trulzcpplolTS3\$R6darksideR6opsTS3rulzskyhighlolopsopsheckl_buzilolheckl_buzilol) >> ~/Academy/En_Decoder/cmake-build-debug/testerDecodeResult.txt
+
+./En_Decoder dict.json -e source_encode.txt -f
+
+./En_Decoder dict.json -d source_decode.txt -f
